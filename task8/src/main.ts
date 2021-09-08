74b13130964d8c685467b80a6281f37df3bc0239
VladyslavMartynov <vladyslav.martynov10@gmail.com>
2021-09-08 23:23:34 +0300

    HW_8

---
+++ task8/src/main.ts
@@ -0,0 +1,228 @@
+// 1 ) Создайте элемент 'p', при клике на котором появляется картинка размером 100px
+// При наведении указателя мышки на картинку ее размер должен плавно увеличиваться до 200px
+// При клике на картинке она должна исчезать
+const p = document.createElement('p');
+const img = document.getElementById('img')!;
+const wrapper = document.getElementById('wrapper')!;
+const inputVal = document.getElementById('inputVal') as HTMLInputElement;
+const btn = document.getElementById('btn')!;
+const invisibleBtn = document.getElementById('invisibleBtn')!;
+const spanText = document.getElementById('spantext')!;
+const box = document.getElementById('box')!;
+const inpCircle = document.getElementById('inpCircle') as HTMLInputElement;
+const btnCircle = document.getElementById('btnCircle')!;
+const circle = document.getElementById('circle')!;
+
+const WRAP_STYLE = `
+    max-width: 900px;
+    width: 100%;
+    margin: 0 auto;
+    margin-bottom: 20px;
+`;
+const P_STYLE = `display: none;`;
+const ZOOM_IMG = `
+    width: 200px;
+    cursor: pointer;
+    transition: 1s;
+    border: 1px solid black;
+`;
+const IMG_STYLE = `
+    cursor: pointer;
+    display: inline-block;
+    margin-top: 50px;
+    width: 100px;
+    height: 100px;
+    border: 1px solid black;
+    transition: 1s;
+`;
+
+const BTN_STYLE = `
+    width: 50px;
+    height: 20px;
+    display: inline-block;
+    border: 1px solid black;
+    background-color: green;
+    color: white;
+    cursor: pointer;
+    text-align: center;
+`;
+
+const INVIS_BTN_STYLE = `
+    width: 70px;
+    height: 20px;
+    display: inline-block;
+    border: 1px solid black;
+    background-color: purple;
+    color: white;
+    cursor: pointer;
+    text-align: center;
+`;
+const BOX_STYLE = `
+    max-width: 900px;
+    width: 100%;
+    margin: 0 auto;
+    position: relative;
+    
+`;
+const CIRCLE_BTN_STYLE = `
+    padding: 5px 30px;
+    text-align: center;
+    position: absolute;
+    cursor: pointer;
+    background-color: #deb887;
+    color: black;
+    text-decoration: none;
+    border-radius: 10px;
+    margin-left: 10px;
+    margin-top: -4px;
+`;
+const CIRCLE_MOVE_STYLE = `
+  position: relative;
+  width: 100px;
+  height: 100px;
+  display: inline-block;
+  border-radius: 50%;
+  background-color: red;
+  cursor: pointer;
+  margin-top: 10px;
+  transition: 3s;
+  
+  
+`;
+const CIRCLE_STYLE = `
+    width: 100px;
+    height: 100px;
+    display: inline-block;
+    border-radius: 50%;
+    background-color: red;
+    cursor: pointer;
+    margin-top: 10px;
+`;
+
+btn.setAttribute('style', BTN_STYLE);
+btn.innerText = "Click";
+
+invisibleBtn.setAttribute('style',INVIS_BTN_STYLE);
+invisibleBtn.innerText = "Invisible";
+
+box.setAttribute('style',BOX_STYLE);
+// inpCircle.setAttribute('style', CIRCLE_INP_STYLE);
+btnCircle.setAttribute('style', CIRCLE_BTN_STYLE);
+circle.setAttribute('style', CIRCLE_STYLE);
+
+wrapper.setAttribute('style', WRAP_STYLE);
+p.innerText = 'Click me!';
+p.style.cursor = 'pointer';
+
+wrapper.prepend(p);
+wrapper.append(img);
+
+const eventP = () => {
+    img.setAttribute('src','images/Patrik.jfif');
+    img.setAttribute('style', IMG_STYLE);
+    p.setAttribute('style', P_STYLE);
+}
+
+const imgZoom = () => {
+    img.setAttribute('style', ZOOM_IMG);
+}
+
+const imgNoZoom = () => {
+    img.setAttribute('style', IMG_STYLE);
+}
+
+const imgDisappear = () => {
+    img.remove();
+}
+
+p.addEventListener('click',eventP);
+img.addEventListener('mouseover', imgZoom);
+img.addEventListener('mouseout', imgNoZoom);
+img.addEventListener('click', imgDisappear);
+
+// Дан массив с числами.
+// Найдите сумму последних N элементов до первого нуля с конца.
+// Пример: [1, 2, 3, 0, 4, 5, 6] - суммируем последние 3 элемента, так как дальше стоит элемент с числом 0.
+
+const array : Array<number> = [1, 2, 3, 0, 4, 5, 6];
+let trigger = 0;
+const result: number = array.reduceRight((acc, item) => {
+    if(item === trigger) {
+        trigger = -1;
+         return acc;
+    } else if(trigger !== -1) {
+        return acc + item;
+    } else {
+        return acc;
+    }
+
+})
+console.log(result);
+
+// Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось больше 10-ти.
+const arr: Array<number> = [1, 2, 3, 4, 5, 6];
+let num:number = 0;
+let resultArr:number = arr.reduce((acc, item) => {
+    if(acc >= 10){
+        return acc;
+    } else {
+        num++;
+        return acc + item;
+    }
+},0)
+
+console.log(num);
+
+//Есть инпут, в который что-то вводим. Рядом с инпутом есть кнопка. По нажатию на кнопку выводим в консоль то, что вписали в инпут.
+// const getValue = (elem: HTMLInputElement) => {
+//     alert(elem.value);
+// }
+
+const handleInp = () => {
+    alert(inputVal.value);
+}
+
+btn.addEventListener('click', handleInp);
+
+//Добавьте JavaScript к кнопке button, чтобы при нажатии элемент
+// исчезал.
+
+const eventText = () => {
+    spanText.innerText =``;
+}
+
+invisibleBtn.addEventListener('click', eventText);
+
+
+const moveCircle = (elem: HTMLElement, value: string): void => {
+    elem.setAttribute('style',`${CIRCLE_MOVE_STYLE};left:${value}px;`);
+}
+
+const validateInput = (elem: HTMLInputElement) => {
+    const trigger = Number.parseInt(elem.value);
+    if(!Number.isNaN(trigger)){
+        moveCircle(circle,elem.value);
+    } else {
+        elem.value = ``;
+    }
+}
+
+const handlePx = () => {
+    validateInput(inpCircle);
+}
+
+btnCircle.addEventListener('click', handlePx);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
